b261bed743ef3fdc0a48b2f0518406dfaaa03d20
lshy1993 <lshy1993@live.com>
2026-08-06 02:10:38 +0900

    keep server host empty

--- config/config.go
+++ config/config.go
@@ -278,7 +278,7 @@ func NewApp() *App {
 		},
 
 		Server: ServerConfig{
-			Host:         getEnv("SERVER_HOST", "0.0.0.0"),
+			Host:         getEnv("SERVER_HOST", ""),
 			Port:         getEnv("PORT", "8080"),
 			ReadTimeout:  getEnvAsDuration("SERVER_READ_TIMEOUT", "30s"),
 			WriteTimeout: getEnvAsDuration("SERVER_WRITE_TIMEOUT", "30s"),
